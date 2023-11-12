0b606aba2d1e98f0513286a22cf79d556219e143
Ljubomir <ljupcedavitkov14@gmail.com>
2023-11-12 17:56:13 +0100

    Lab 2 step6

---
+++ LAB1/LAB1/src/main/java/mk/finki/ukim/mk/lab/service/ProductionService.java
@@ -0,0 +1,6 @@
+package mk.finki.ukim.mk.lab.service;
+import java.util.List;
+import mk.finki.ukim.mk.lab.model.Production;
+public interface ProductionService {
+    public List<Production> findAll();
+}
